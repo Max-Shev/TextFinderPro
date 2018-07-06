993103cd2177dac40cf4242f52fee50d82cbb4e7
Max <maximka.shevelyov@yandex.ru>
2018-07-06 22:37:46 +0300

    add extracted file text class

---
+++ TextFinderPro/TextFinderPro.Library/TextExtraction/ExtractedFileText.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFinderPro.Library.TextExtraction
+{
+    /// <summary>
+    /// Result of extraction text from file
+    /// </summary>
+    public class ExtractedFileText
+    {
+        /// <summary>
+        /// Extracted text
+        /// </summary>
+        public string FileText { get; }
+
+        /// <summary>
+        /// File of text
+        /// </summary>
+        public string FileName { get; }
+
+        /// <summary>
+        /// Mark of succes extraction
+        /// </summary>
+        public bool IsValidExtraction { get; }
+
+        public ExtractedFileText(string fileText, string fileName, bool isValidExtraction)
+        {
+            FileText = fileText;
+            FileName = fileName;
+            IsValidExtraction = isValidExtraction;
+        }
+
+    }
+}
