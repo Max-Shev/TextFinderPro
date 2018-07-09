bdbee051fc3d5aa11650371b46e20fa97850eb91
Max <maximka.shevelyov@yandex.ru>
2018-07-09 23:14:10 +0300

    add exact search provider tests

---
+++ TextFinderPro/TextFinderPro.Tests/Searching/ExactSearchProviderTest.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextFinderPro.Library.Searching.SearchProviders;
+
+namespace TextFinderPro.Tests.Searching
+{
+    [TestFixture]
+    public class ExactSearchProviderTest
+    {
+        private SearchProvider _exactSearchProvider;
+        private string _sourceText;
+        [SetUp]
+        public void Init()
+        {
+            _sourceText = "Карл у Клары украл кораллы, а Клара у Карла украла кларнет.";
+            _exactSearchProvider = SearchProviderFactory.GetSearchProvider(SearchProviderType.ExactSearchProvider , _sourceText);
+        }
+        #region SearchOne test methods
+        public void KarlHasPosition0()
+        {
+            // arrange
+            var searchResult = _exactSearchProvider.SearchOne("Karl").First();
+
+            //assert
+            Assert.AreEqual(searchResult.Position, 0);
+        }
+
+        public void TextHasTwoIncommings()
+        {
+            // arrange
+            var searchResult = _exactSearchProvider.SearchOne("Karl").Count();
+
+            //assert
+            Assert.AreEqual(searchResult, 2);
+        }
+
+        public void SearchOfKarlReturnedTextKarl()
+        {
+            // arrange
+            var searchResult = _exactSearchProvider.SearchOne("Karl").First();
+
+            //assert
+            Assert.AreEqual(searchResult.Text, "Karl");
+        }
+        #endregion
+
+        #region SearchAny test methods
+        public void TextHasAnyKlarAndNet()
+        {
+            // arrange
+            var searchResult = _exactSearchProvider.SearchAny("Klar" , "Net").Count();
+
+            //assert
+            Assert.AreNotEqual(searchResult, 0);
+        }
+
+        #endregion
+
+        #region  SearchAll test methods
+
+        #endregion
+    }
+}
