01f099208df2a4e121c15e143896a6726adcb8d3
Max <maximka.shevelyov@yandex.ru>
2018-07-09 21:43:09 +0300

    add search result class

---
+++ TextFinderPro/TextFinderPro.Library/Searching/SearchProviders/SearchResult/SearchResult.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFinderPro.Library.Searching.SearchProviders.SearchResullt
+{
+    /// <summary>
+    /// Result of search in file
+    /// </summary>
+    public class SearchResult
+    {
+        /// <summary>
+        /// Searched file name
+        /// </summary>
+        public string FileName { get; set; }
+
+        /// <summary>
+        /// Mark of valid extraction
+        /// </summary>
+        public bool IsValid { get; set; }
+
+        /// <summary>
+        /// Type of search (exact or regex etc.)
+        /// </summary>
+        public SearchProviderType SearchType { get; set; }
+
+        /// <summary>
+        /// Found text incomings
+        /// </summary>
+        public IEnumerable<FoundText> FoundTexts { get; set; }
+
+        /// <summary>
+        /// Return true if there is some search success in file
+        /// </summary>
+        public bool HsaFounds => FoundTexts.Count() != 0;
+    }
+}
