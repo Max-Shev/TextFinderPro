f66456c43785d54901266be1af43fc29b563b814
Max <maximka.shevelyov@yandex.ru>
2018-07-09 22:39:28 +0300

    add exact search provider class

---
+++ TextFinderPro/TextFinderPro.Library/Searching/SearchProviders/ExactSearchProvider.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextFinderPro.Library.Searching.SearchProviders.SearchResullt;
+
+namespace TextFinderPro.Library.Searching.SearchProviders
+{
+    public class ExactSearchProvider : SearchProvider
+    {
+        public ExactSearchProvider(string sourceText) : base(SearchProviderType.ExactSearchProvider, sourceText)
+        { }
+
+
+        public override IEnumerable<FoundText> SearchAll(IEnumerable<string> patterns)
+        {
+            return patterns.SelectMany(SearchOne);
+        }
+
+        public override IEnumerable<FoundText> SearchAny(IEnumerable<string> patterns)
+        {
+            IEnumerable<FoundText> foundTexts = null;
+            foreach (var pattern in patterns)
+            {
+                foundTexts = SearchOne(pattern);
+                if (foundTexts.Count() != 0) return foundTexts;
+            }
+            return foundTexts;
+        }
+
+        public override IEnumerable<FoundText> SearchOne(string pattern)
+        {
+            int startSearchIndex = _sourceText.IndexOf(pattern);
+            while (startSearchIndex != -1)
+            {
+                yield return (new FoundText(pattern, startSearchIndex));
+                startSearchIndex = _sourceText.IndexOf(pattern, startSearchIndex + 1);
+            }
+        }
+    }
+}
