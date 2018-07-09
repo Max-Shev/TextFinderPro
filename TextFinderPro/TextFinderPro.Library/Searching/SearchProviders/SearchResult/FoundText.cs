691c42793c01b34c1e2b2b2b5804bc660cdbc383
Max <maximka.shevelyov@yandex.ru>
2018-07-09 21:42:44 +0300

    add found text class

---
+++ TextFinderPro/TextFinderPro.Library/Searching/SearchProviders/SearchResult/FoundText.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFinderPro.Library.Searching.SearchProviders.SearchResullt
+{
+    /// <summary>
+    /// Found text with position. Position = -1 of it's uknown, or text doesn't exist in file
+    /// </summary>
+    public class FoundText
+    {
+        public const int NotFoundPosition = -1;
+
+        public string Text { get; set; }
+        public int Position { get; set; }
+
+        public FoundText(string text, int position = NotFoundPosition)
+        {
+            Text = text;
+            Position = position;
+        }
+    }
+}
