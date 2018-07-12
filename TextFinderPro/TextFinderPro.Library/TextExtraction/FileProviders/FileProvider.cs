cf1ee065f07ee2dabc72c57e1c1c12fc85a8c779
Max <maximka.shevelyov@yandex.ru>
2018-07-12 22:23:16 +0300

    add file provider base class and type

---
+++ TextFinderPro/TextFinderPro.Library/TextExtraction/FileProviders/FileProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFinderPro.Library.TextExtraction.FileProviders
+{
+    abstract class FileProvider
+    {
+        public FileProviderType FileProviderType { get; }
+
+        protected FileProvider(FileProviderType fileProviderType)
+        {
+            FileProviderType = fileProviderType;
+        }
+
+        public abstract IEnumerable<ExtractedFileText> GetTextFromFile(string filePath);
+        public abstract IEnumerable<ExtractedFileText> GetTextFromFiles(IEnumerable<string> filePath);
+    }
+}
