55b90f097c66e11ec63ca1c317dcd6fb55225d58
Max <maximka.shevelyov@yandex.ru>
2018-07-12 22:47:53 +0300

    add text file provider class

---
+++ TextFinderPro/TextFinderPro.Library/TextExtraction/FileProviders/TextFileProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFinderPro.Library.TextExtraction.FileProviders
+{
+    public class TextFileProvider : FileProvider
+    {
+        protected TextFileProvider(FileProviderType fileProviderType) : base(FileProviderType.TextFile)
+        {
+        }
+
+        public override IEnumerable<ExtractedFileText> GetTextFromFile(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<ExtractedFileText> GetTextFromFiles(IEnumerable<string> filePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
