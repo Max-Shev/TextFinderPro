bb21c0bfd02164c2a85fc234fea28ec54cad0823
Max <maximka.shevelyov@yandex.ru>
2018-07-14 10:46:57 +0300

    Add initializing of pdf file provider test

--- TextFinderPro/TextFinderPro.UnitTests/TextExtraction/PdfFileProviderTest.cs
+++ TextFinderPro/TextFinderPro.UnitTests/TextExtraction/PdfFileProviderTest.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TextFinderPro.Library.TextExtraction;
 using TextFinderPro.Library.TextExtraction.FileProviders;
 
 namespace TextFinderPro.UnitTests.TextExtraction
@@ -11,6 +12,30 @@ namespace TextFinderPro.UnitTests.TextExtraction
     [TestFixture]
     public class PdfFileProviderTest
     {
-        PdfFileProvider _pdfFileProvider = new PdfFileProvider();
+        private PdfFileProvider _pdfFileProvider;
+        private ExtractedFileText _extractedFileText;
+        private string _extractedText => _extractedFileText.FileText;
+        private const string PdfFileForTest = "TestFiles/test.pdf";
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            _pdfFileProvider = new PdfFileProvider();
+            _extractedFileText = _pdfFileProvider.GetTextFromFile(PdfFileForTest);
+        }
+
+        [Test]
+        public void ExtractionIsValid()
+        {
+            var result = _pdfFileProvider.GetTextFromFile(PdfFileForTest);
+            Assert.IsTrue(result.IsValidExtraction);
+        }
+        
+        [Test]
+        public void FileConstainsPolis()
+        {
+            var result = _extractedText.Contains("полис");
+            Assert.IsTrue(result);
+        }
     }
 }
