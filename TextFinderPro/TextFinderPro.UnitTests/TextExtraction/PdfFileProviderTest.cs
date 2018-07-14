09802b4809ace1f34a166caaffa05b5d0f22092a
Max <maximka.shevelyov@yandex.ru>
2018-07-14 10:31:26 +0300

    Add test for file providers

---
+++ TextFinderPro/TextFinderPro.UnitTests/TextExtraction/PdfFileProviderTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextFinderPro.Library.TextExtraction.FileProviders;
+
+namespace TextFinderPro.UnitTests.TextExtraction
+{
+    [TestFixture]
+    public class PdfFileProviderTest
+    {
+        PdfFileProvider _pdfFileProvider = new PdfFileProvider();
+    }
+}
