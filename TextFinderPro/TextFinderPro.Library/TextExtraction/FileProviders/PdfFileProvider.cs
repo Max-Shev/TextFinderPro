91bb8043019732ae8de47a464f62119974bb0ceb
Max <maximka.shevelyov@yandex.ru>
2018-07-14 09:57:52 +0300

    add pdf file provider

---
+++ TextFinderPro/TextFinderPro.Library/TextExtraction/FileProviders/PdfFileProvider.cs
@@ -0,0 +1,30 @@
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFinderPro.Library.TextExtraction.FileProviders
+{
+    public class PdfFileProvider : FileProvider
+    {
+        protected PdfFileProvider(FileProviderType fileProviderType) : base(FileProviderType.PdfFile)
+        {
+        }
+
+        protected override string TryGetTextFromFile(string filePath)
+        {
+            PdfReader pdfReader = new PdfReader(filePath);
+            StringBuilder pdfTextBuilder = new StringBuilder();
+            for (int pageIndex = 1; pageIndex < pdfReader.NumberOfPages; pageIndex++)
+            {
+                ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+                string text = PdfTextExtractor.GetTextFromPage(pdfReader, pageIndex, strategy);
+                pdfTextBuilder.Append(text);
+            }
+            return pdfTextBuilder.ToString();
+        }
+    }
+}
