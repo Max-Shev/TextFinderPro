a2e45f73b9a5da9655ca6370e33f934b3a8a4231
Max <maximka.shevelyov@yandex.ru>
2018-07-19 22:26:58 +0300

    delete unusing directives

--- TextFinderPro/TextFinderPro.UnitTests/TextExtraction/TextFileProviderTest.cs
+++ TextFinderPro/TextFinderPro.UnitTests/TextExtraction/TextFileProviderTest.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TextFinderPro.Library.TextExtraction.FileProviders;
 
 namespace TextFinderPro.UnitTests.TextExtraction
 {
-    public class TextFileProviderTest
+    public class TextFileProviderTest:FileProviderBaseTest
     {
+        [OneTimeSetUp]
+        public void Init()
+        {
+            _fileProvider = new TextFileProvider();
+            _fileForTest = Path.Combine(TestFilesFolder, "test.txt");
+            _extractedFileText = _fileProvider.GetTextFromFile(_fileForTest);
+        }
+
+        [Test]
+        public void TxtExtractionIsValid()
+        {
+            var result = _fileProvider.GetTextFromFile(_fileForTest);
+            Assert.IsTrue(result.IsValidExtraction);
+        }
+
+        [Test]
+        public void FileContainsInstall()
+        {
+            var result = _extractedText.Contains("install");
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void FileNotContainsInstallee()
+        {
+            var result = _extractedText.Contains("installee");
+            Assert.IsFalse(result);
+        }
     }
 }
