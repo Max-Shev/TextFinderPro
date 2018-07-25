5306ed1c156c6df82f611f1ad857d49739848a47
Max <maximka.shevelyov@yandex.ru>
2018-07-25 20:50:33 +0300

    Add regex search provider test

---
+++ TextFinderPro/TextFinderPro.UnitTests/Searching/RegexSearchProviderTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextFinderPro.Library.Searching.SearchProviders;
+
+namespace TextFinderPro.UnitTests.Searching
+{
+    [TestFixture]
+    public class RegexSearchProviderTest
+    {
+        private SearchProvider _regexSearchProvider;
+        private string _sourceText;
+
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            _sourceText = "Карл у Клары украл кораллы, а Клара у Карла украла кларнет.";
+            _regexSearchProvider = SearchProviderFactory.GetSearchProvider(SearchProviderType.RegexSearchProvider, _sourceText);
+        }
+
+        [Test]
+        public void TextHasKarlIncomming()
+        {
+            var searchResult = _regexSearchProvider.SearchOne("Карл");
+            Assert.IsNotEmpty(searchResult);
+        }
+    }
+}
