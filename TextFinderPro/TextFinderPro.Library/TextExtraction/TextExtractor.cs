11ce4096cec35920d79f54d4250708d3c81d4b3a
Max <maximka.shevelyov@yandex.ru>
2018-07-30 22:56:37 +0300

    Add text extractor class

---
+++ TextFinderPro/TextFinderPro.Library/TextExtraction/TextExtractor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFinderPro.Library.TextExtraction
+{
+    public class TextExtractor
+    {
+        public bool AllFilesNeedToExtract { get; set; }
+        public ICollection<string> ExtentionsForExtracting { get; set; }
+        public ICollection<string> ExcludedExtentions { get; set; }
+    }
+}
